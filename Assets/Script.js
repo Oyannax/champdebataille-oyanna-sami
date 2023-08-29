5f1f93356cfcbad4f8f9e0c3fe9257a3fc9ba616
Oyanna Lopez <oyannalopez14@gmail.com>
2023-08-29 10:00:56 +0200

    create players

---
+++ Assets/Script.js
@@ -0,0 +1,32 @@
+let players = [
+    "Thomas", {
+        pv: 50,
+        xp: 8,
+        weapon: 6,
+        shield: 7
+    },
+    "Brice", {
+        pv: 50,
+        xp: 7,
+        weapon: 8,
+        shield: 4
+    },
+    "Florian", {
+        pv: 50,
+        xp: 10,
+        weapon: 5,
+        shield: 8
+    },
+    "Aurélien", {
+        pv: 50,
+        xp: 6,
+        weapon: 9,
+        shield: 8
+    }
+]
+
+console.log(players[1])
+
+function getRandomPlayer() {
+
+}
