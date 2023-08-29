45eaf190f0593a879147714d7be82b6e51456e09
Sami Moumry <samimoumry0@gmail.com>
2023-08-29 11:25:06 +0200

    improve function getRandomPlayer

--- Assets/Script.js
+++ Assets/Script.js
@@ -32,9 +32,29 @@ let players = [
 
 function getRandomPlayer() {
     let defender;
-    let attacker = Math.floor(Math.random() * players.length );
-    while (attacker === defender || defender === undefined){
+    let attacker;
+    while (attacker === undefined || players[attacker].pv <= 0) {
+    attacker = Math.floor(Math.random() * players.length );
+    }
+    while (attacker === defender || defender === undefined || players[defender].pv <= 0) {
         defender = Math.floor(Math.random() * players.length);
     }
     return [players[attacker], players[defender]];
 }
+
+console.table(getRandomPlayer());
+
+/*function getBattle(){
+    getRandomPlayer();
+    let attackerScore = players[attacker].xp + Math.floor(Math.random() * (players[attacker].weapon + 1));
+    let defenderScore = players[defender].xp + Math.floor(Math.random() * (players[defender].shield + 1));
+    if (attackerScore > defenderScore && players[defender].pv > 0){
+        players[defender].pv -= attackerScore;
+        return `Le joueur ${players[attacker].name} a attaqué avec un score de ${attackerScore}. ${players[defender].name} s'est défendu avec un score de ${defenderScore}. ${players[attacker].name} a gagné le combat!`;
+    }
+    else if (players[defender].pv <= 0){
+        return `Le joueur ${players[attacker].name} a attaqué avec un score de ${attackerScore}. ${players[defender].name} s'est défendu avec un score de ${defenderScore}. ${players[attacker].name} a gagné le combat et le joueur ${players[defender].name} est mort!`;
+    }
+    return `Le joueur ${players[attacker].name} a attaqué avec un score de ${attackerScore}. ${players[defender].name} s'est défendu avec un score de ${defenderScore}. ${players[attacker].name} a perdu le combat!`;
+}
+*/
